fd6bfdd3b281699f924003af0979376080580456
Pelle Andersson <pellejamas@gmail.com>
2021-04-09 15:16:46 +0200

    Version 2: SD-fil: Uppdaterad med random-username och random-mail

--- mailchimp/src/test/java/stepDefinitions/StepDefinitions.java
+++ mailchimp/src/test/java/stepDefinitions/StepDefinitions.java
@@ -2,6 +2,8 @@ package stepDefinitions;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,29 +19,42 @@ import io.cucumber.java.en.When;
 public class StepDefinitions {
 
 	private WebDriver driver;
-
+	private Random rand = new Random();
+	
 	@Before
 	public void openBrowser() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Program Files\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver(); 													// Startar Chrome
 		driver.get("https:login.mailchimp.com/signup"); 								// Navigerar till mailchimp.com
 		Thread.sleep(1000); 															// Vänta 1 sek
 		driver.findElement(By.id("onetrust-accept-btn-handler")).click(); 				// Klickar på Accept All Cookies
 	}
 
-	@Given("I have entered {string} as a mail")
-	public void i_have_entered_string_as_a_mail(String String1) throws InterruptedException {
-		WebElement email = driver.findElement(By.id("email")); 							// Hittar email-rutan
-		email.sendKeys("Hejhej@mail.com"); 												// Skriver in en email
-		Thread.sleep(1000); 															// Vänta 1 sek
+	@Given("I have entered {string} as a mail or {string} randomMail")
+	public void i_have_entered_string_as_a_mail_or_string_randomMial(String mail, String random) throws InterruptedException {
+		if (random.equals("0")){
+			WebElement email = driver.findElement(By.id("email")); 						// Hittar email-rutan
+			email.sendKeys(mail);														// Skriver in ett email
+			Thread.sleep(1000); 														// Vänta 1sek
+		}
+		if (random.equals("1")) {
+			Thread.sleep(1000); 														// Vänta 1sek
+			WebElement email = driver.findElement(By.id("email")); 						// Hittar email-rutan
+			email.sendKeys(rand+mail.substring(5));										// Skriver in en randomEmail
+		}
 	}
-
-	@And("I have also entered {string} as an username")
-	public void i_have_also_entered_string_as_an_username(String String1) throws InterruptedException {
-		WebElement user = driver.findElement(By.id("new_username")); 					// Hittar användarnamnet-rutan
-		user.sendKeys("pollens1"); 														// Skriver in ett användarnamn
-		Thread.sleep(1000); 															// Vänta 1 sek
+	@And("I have also entered {string} as an username or {string} randomUsername")
+	public void i_have_also_entered_string_as_an_username_or_string_randomUsername(String username, String random) throws InterruptedException {
+		if (random.equals("0")){
+			WebElement user = driver.findElement(By.id("new_username")); 				// Hittar användarnamn-rutan
+			user.sendKeys(username);						  							// Skriver in ett Användernamn
+			Thread.sleep(1000); 							  							// Vänta 1sek
+		}
+		if (random.equals("1")) {
+			Thread.sleep(1000); 							  							// Vänta 1sek
+			WebElement user = driver.findElement(By.id("new_username")); 		  		// Hittar användarnamn-rutan
+			user.sendKeys(rand.nextInt(100)+username+rand.nextInt(100));				// Skriver in ett randomAnvändernamn
+		}
 	}
 
 	@And("I have also entered {string} as an password")
